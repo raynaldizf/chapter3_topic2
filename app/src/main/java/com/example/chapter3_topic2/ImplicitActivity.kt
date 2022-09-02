7b41dd0d560c6c9dfa9455e4afad40a515055af3
surezluvy <raynaldizoel@gmail.com>
2022-09-02 21:08:56 +0700

    Implicit Activity

--- app/src/main/java/com/example/chapter3_topic2/ImplicitActivity.kt
+++ app/src/main/java/com/example/chapter3_topic2/ImplicitActivity.kt
@@ -1,13 +1,30 @@
 package com.example.chapter3_topic2
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_implicit.*
 
 class ImplicitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_implicit)
 
-        
+        btnGallery.setOnClickListener{
+            val pindah = Intent(Intent.ACTION_VIEW)
+            pindah.data = Uri.parse("content://media/external/images/media/")
+            startActivity(pindah)
+        }
+        btnCamera.setOnClickListener{
+            val pindah = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(pindah)
+        }
+        btnWeb.setOnClickListener{
+            val pindah = Intent(Intent.ACTION_VIEW)
+            pindah.data = Uri.parse("https://www.binaracademy.com/")
+            startActivity(pindah)
+        }
     }
 }
